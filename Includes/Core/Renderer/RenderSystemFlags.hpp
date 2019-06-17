3a674791b615144736021b34c34cc4f2135f410a
Chris Ohk <utilForever@gmail.com>
2019-06-17 13:18:00 +0900

    feat(core): Add struct 'RenderSystemDescriptor'

---
+++ Includes/Core/Renderer/RenderSystemFlags.hpp
@@ -0,0 +1,14 @@
+#ifndef CUBBYRENDER_RENDER_SYSTEM_FLAGS_H
+#define CUBBYRENDER_RENDER_SYSTEM_FLAGS_H
+
+#include <string>
+
+namespace CubbyRender
+{
+struct RenderSystemDescriptor
+{
+    std::string moduleName;
+};
+}  // namespace CubbyRender
+
+#endif
